1f85643fcce26003db9c708c9a5a82baa255347c
Phillip Kovalev <phillipkova@gmail.com>
2018-11-02 12:05:34 -0400

    responsive swiper slider

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -14,20 +14,16 @@ var swiper = new Swiper('.swiper-container', {
   slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   breakpoints: {
     576: {
-      slidesPerView: 1
+      slidesPerView: 1.3
     },
     992: {
-      slidesPerView: 2
+      slidesPerView: 2.2
     },
     1200: {
       slidesPerView: 3
