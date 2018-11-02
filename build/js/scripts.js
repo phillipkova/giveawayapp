6251a2c5c69a7cd4e8e559076b69295914ac9d5e
Phillip Kovalev <phillipkova@gmail.com>
2018-11-02 11:18:49 -0400

    install swiper slider

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -6,3 +6,31 @@ $(document).ready(function() {
 $('[data-toggle="tooltip"]').tooltip('toggleEnabled')
 
 console.log('hi2');
+
+// swiper init
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 15,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 1
+    },
+    992: {
+      slidesPerView: 2
+    },
+    1200: {
+      slidesPerView: 3
+    }
+  }
+});
